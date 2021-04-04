2ddbb90e7e39c73095a011bead5021459b2dcae4
kalagxw <kalagxw@gmail.com>
2021-04-04 17:47:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome lede/package/lean/
