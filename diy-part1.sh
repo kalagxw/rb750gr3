a35863d28e07fde4fad6b8ee2056f86f0cd9db57
kalagxw <kalagxw@gmail.com>
2022-03-20 21:21:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/ntfs3-mount/antfs-mount/' package/lean/automount/Makefile
+#sed -i 's/ntfs3-mount/antfs-mount/' package/lean/automount/Makefile
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
